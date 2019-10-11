ebf5ea7cda86b0d5f66ae5dcdafcbbef41b6a55f
nate mccay <nate.mccay@ymail.com>
2019-10-11 11:49:26 -0500

    test

---
+++ ExampleOfClasses/ExampleOfClasses/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExampleOfClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var product = new Product();
+            product.Name = "Nintendo Switch";
+            var name = product.Name;
+            product.Price = 299.99;
+            var price = product.Price;
+            Console.WriteLine($"${price} {name}");
+        }
+    }
+}
